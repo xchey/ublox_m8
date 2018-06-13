7209010d2e2c2befc26ec96fd3e90d10f184c4f2
xchey <35334330+xchey@users.noreply.github.com>
2018-06-13 22:06:44 +0800

    some nav solutions

--- read_raw.py
+++ read_raw.py
@@ -9,7 +9,7 @@
 """
 import sys
 import serial
-from struct import pack, unpack, unpack_from, calcsize
+from struct import unpack, unpack_from, calcsize
 
 
 # protocol constants
@@ -24,13 +24,14 @@ CLS_CFG = 0x06
 
 # ublox messag ID
 MSG_RAWX    = 0x15
+MSG_SFRBX   = 0x13
 MSG_POSECEF = 0x01
 MSG_VELECEF = 0x11
 MSG_TIMEGPS = 0x20
 
-class StructField:
+class UbxStructField:
     '''
-    Descriptor representing a simple structure field
+    Descriptor representing a simple struct field
     '''
     def __init__(self, format, offset):
         self.format = format
@@ -41,75 +42,128 @@ class StructField:
             return self
         else:
             r = unpack_from(self.format, instance._buffer, self.offset)
+            return r[0] if len(r) == 1 else r
 
-class decode_meta(type):
+class UbxStructureMeta(type):
     '''
-    Metaclass that automatically creates StructField descriptors
+    Meta class that automatically creates StructureField descriptor
     '''
     def __init__(self, clsname, bases, clsdict):
-        super().__init__(self)
         fields = getattr(self, '_fields_', [])
-
         byte_order = ''
         offset = 0
         for format, fieldname in fields:
             if format.startswith(('<', '>', '!', '@')):
                 byte_order = format[0]
-
-        format = format[1:]
-        format = byte_order + format
-
-        setattr(self, fieldname, StructField(format, offset))
-        offset += calcsize(format)
+                format = format[1:]
+            format = byte_order + format
+            setattr(self, fieldname, UbxStructField(format, offset))
+            offset += calcsize(format)
         setattr(self, 'struct_size', offset)
 
-class decode_msg(metaclass=decode_meta):
+#
+class Structure(metaclass=UbxStructureMeta):
+    '''
+    Structure to read and decode ublox data
+    '''
     def __init__(self, bytedata):
-        self._buffer = bytedata
+        self._buffer = memoryview(bytedata)
 
     @classmethod
     def from_file(cls, f):
         return cls(f.read(cls.struct_size))
 
+    @classmethod
+    def from_ser(cls, ser):
+        return cls(ser.read(cls.struct_size))
 
-class ubx_desp:
+    @classmethod
+    def from_buff(cls, buf):
+        return cls(buf)
+
+
+class UBloxError(Exception):
+    '''Ublox error class'''
+    def __init__(self, msg):
+        Exception.__init__(self, msg)
+        self.message = msg
+
+
+class UbloxDesp():
     '''
-    class to describe the structure of ublox message
+    Class to describe the structure of ublox message
     '''
-    def __init__(self, name, msg_format, fields=None, count_field=None, format2=None, fields2=None):
-        self.name = name
-        self.msg_format = msg_format
-        if fields is None:
+    def __init__(self, data, *args):
+
+        self._data = data
+        self.name = args[0][0]
+
+        fmt = []; self._fields_ = []
+        for i in range(1, len(args[0][1])):
+            fmt.append(args[0][1][i])
+        fmt[0] = '<' + fmt[0]
+
+        self.msg_format = fmt
+        if args[0][2] is None:
             self.fields = []
         else:
-            self.fields = fields
-        self.count_field = count_field
-        self.format2 = format2
-        self.fields2 = fields2
+            self.fields = args[0][2]
 
-    # def decode(self, msg):
-    #     format =
+        for flt, fld in zip(fmt, self.fields):
+            self._fields_.append((flt, fld))
+
+        if len(args[0]) > 3:
+            self.count_field = args[0][3]
+            self.format2 = args[0][4]
+            self.fields2 = args[0][5]
+
+    def decode(self):
+        '''
+        unpack a UbloxMessage with specified fields
+        TBD
+        '''
+        class DecHed(Structure):
+            _fields_ = self._fields_
+        phead = DecHed.from_buff(self._data)
+        return self.name, phead
+
 
 # message types
-msg_type = {
-    # (CLS_RXM, MSG_RAWX) : ubx_desp('RXM_RAWX',
-    #                                    ['']),
+dmsg_type = {
+    (CLS_RXM, MSG_RAWX) :   ('RXM_RAWX',
+                             '<dHbBbb',
+                             ['rcvTow', 'week', 'leapS', 'numMeas', 'recStat', 'resv1'],
+                             'numMeas',
+                             '<ddfBBBBHBbbbbB',
+                             ['prMes', 'cpMes', 'doMes', 'gnssId', 'svId', 'resv2', 'freqId',
+                              'locktime', 'cno', 'prStdev', 'cpStdev', 'doStdev', 'trkStat',
+                              'resv3']),
 
-    (CLS_NAV, MSG_POSECEF): ubx_desp('NAV_POSECEF',
-                                     '<LlllL',
-                                     ['iTOW', 'ecefX', 'ecefY', 'ecefZ', 'pAcc']),
+    (CLS_RXM, MSG_SFRBX) : ('RXM_SFRBX',
+                            '<BBBBBBBB',
+                            ['gnssId', 'svId', 'resv1', 'freqId', 'numWords', 'resv2', 'ver', 'resv3'],
+                            'numWords',
+                            '<L',
+                            ['dwrd']),
 
-    (CLS_NAV, MSG_VELECEF): ubx_desp('NAV_VELECEF',
-                                     '<LlllL',
-                                     ['iTOW', 'ecefVX', 'ecefVY', 'ecefVZ', 'sAcc']),
+    (CLS_NAV, MSG_POSECEF): ('NAV_POSECEF',
+                             '<LlllL',
+                             ['iTOW', 'ecefX', 'ecefY', 'ecefZ', 'pAcc']),
 
-    (CLS_NAV, MSG_TIMEGPS): ubx_desp('NAV_TIMEGPS',
-                                     '<LlhbBL',
-                                     ['iTOW', 'fTOW', 'week', 'leapS', 'valid', 'tAcc'])
+    (CLS_NAV, MSG_VELECEF): ('NAV_VELECEF',
+                             '<LlllL',
+                             ['iTOW', 'ecefVX', 'ecefVY', 'ecefVZ', 'sAcc']),
+
+    (CLS_NAV, MSG_TIMEGPS): ('NAV_TIMEGPS',
+                             '<LlhbBL',
+                             ['iTOW', 'fTOW', 'week', 'leapS', 'valid', 'tAcc'])
 }
 
-class ublox:
-    def __init__(self, port, baudrate=115200, timeout=0, bytesize=8, stopsize=1,
+class Ublox:
+    '''
+    Ublox class, init with serial port object
+    '''
+    def __init__(self, port, baudrate=9600, timeout=0, bytesize=8, stopsize=1,
                  parity='N'):
         self.port = port
         self.baud = baudrate
@@ -128,38 +182,102 @@ class ublox:
                 sys.stderr.write('Could not open serial port {}:{}\n'.format(self.port, e))
                 sys.exit(1)
 
-    def read_raw(self, n):
+    def read_raw(self, rdline=False):
         '''
         read n bytes from serial port
-        :param n:
-        :return:
         '''
-        return self.ser.read(n)
+        raw_data = b''
+        data = b''
 
-    def decode_prx(self):
-        '''
-        read serial message preamble
-        :return: class id, message id, message length
-        '''
-        prex = self.ser.read(6)
-        if prex[0] == PREAMBLE1 and prex[1] == PREAMBLE2:
-            return prex[2], prex[3], unpack('<H', prex[4:])
+        try:
+            chunks = iter(lambda : self.ser.read(), b'\xb5')
+            for chk in chunks:
+                raw_data += chk
+        except serial.SerialException as e:
+            sys.stderr.write('Could not read from serial port {}:{}\n'.format(self.port, e))
+
+        # print(raw_data)
+        raw_len = len(raw_data)
+
+        if raw_data == b'' :
+            data = b''
+        elif raw_data[0] == PREAMBLE2:
+            # raw data field without sync char 1, add b'\xb5'
+            # while in concat bytes.
+            # (sync char 2, class, id, length, ck_a, ck_b) = 7
+            # rxm_rawx still need (msg_len - raw_len + 7)
+            msg_len, = unpack('<H', raw_data[3:5])
+            if msg_len == raw_len-7:
+                data = raw_data
+            else:
+                data = raw_data + b'\xb5' + self.ser.read(msg_len-raw_len+7)
+                # print(data, '\n', 'final length: ', len(data) - 7, ',msg len: ', msg_len, ',read msg len: ',
+                #       msg_len-raw_len+7, ',current msg len: ', raw_len-5)
         else:
-            return None
+            print(raw_data[0], ' Something wrong with serial read')
+        return data
 
     def decode_raw(self):
         '''
         decode ublox binary message
-        :return: decoded data
         '''
-        cls_id, msg_id, dlen = self.decode_prx()
-        print(cls_id, msg_id, dlen)
+        data = self.read_raw()
+        if len(data) > 1:
+            cls_id, msg_id, dlen = unpack('<BBH', data[1:5])
+
+            print(cls_id, msg_id, dlen)
+            if self.check_sum(data):
+                print('Check sum passed!')
+
+                ## decode binary data
+                # dec = UbloxDesp(data[5:-2], dmsg_type[(cls_id, msg_id)])
+                # name, fld = dec.decode()
+                #
+                # if msg_id == MSG_TIMEGPS:
+                #     print(name, fld.iTOW, fld.fTOW, fld.week, fld.leapS, bin(fld.valid), fld.tAcc)
+                # elif msg_id == MSG_VELECEF:
+                #     print(name, fld.iTOW, fld.ecefVX, fld.ecefVY, fld.ecefVZ, fld.sAcc)
+                # elif msg_id == MSG_POSECEF:
+                #     print(name, fld.iTOW, fld.ecefX, fld.ecefY, fld.ecefZ, fld.pAcc)
+                # dec.show()
+                # dec.from_buff(data)
 
 
+            else:
+                print('Check sum failure!')
+                return False
+        else:
+            # print('Null ublox raw data')
+            return False
 
+    def check_sum(self, dat):
+        '''
+        ublox check sum calculation
+        '''
+        cck_a = 0; cck_b = 0
+        for i in dat[1:-2]:
+            cck_a = (cck_a + i) & 0xFF
+            cck_b = (cck_b + cck_a) & 0xFF
 
+        ck_a, ck_b = unpack('<BB', dat[-2:])
+        if ck_a == cck_a and ck_b == cck_b:
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
-    dev = ublox('com23', 115200)
+    dev = Ublox('com12', 115200)
     while True:
         dev.decode_raw()
+    # ser = serial.Serial('com12', 115200)
+    # ser.close()
+    # ser.open()
+    # while True:
+    #     chunks = iter(lambda : ser.read(2), pack('<BB', PREAMBLE1, PREAMBLE2))
+    #     b = b''
+    #     for chk in chunks:
+    #         b += chk
+    #     print(b)
+    #     b = b''
+
+
